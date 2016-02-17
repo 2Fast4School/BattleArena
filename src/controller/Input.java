5d25bc2d27700729fd749c1f01380e8c356f9128
Victor Dahlberg <v.dahlberg01@gmail.com>
2016-02-17 21:27:06 +0100

    Enkel javadoc i Input, behöver nog lägga till mer. Kommentarer hur uträkningen av vinkeln fungerar.

--- src/controller/Input.java
+++ src/controller/Input.java
@@ -7,11 +7,23 @@ import java.awt.event.MouseMotionListener;
 
 import model.Player;
 
-
+/**
+ * <h1>Input</h1>
+ * Input is the class that handles all the input in this game.
+ * It implements the interfaces KeyListener and MouseMotionListener.
+ * 
+ * Since the input class changes the status of a player, a player model is needed.
+ * Initialize the player with the setup method.
+ * 
+ */
 public class Input implements KeyListener, MouseMotionListener{
 	private Player p;
 	private final int VELOCITY = 1;
 
+	/**
+	 * Initiliazes the Input object with the specified Player object.
+	 * @param Player p The player object that the Class will manipulate.
+	 */
 	public void setup(Player p){this.p = p;}
 	
 	public void keyPressed(KeyEvent e) {
@@ -56,14 +68,21 @@ public class Input implements KeyListener, MouseMotionListener{
 
 	public void keyTyped(KeyEvent e) {}
 
-	public void mouseDragged(MouseEvent arg0) {
+	public void mouseDragged(MouseEvent e) {
 		
 	}
-
+	
 	public void mouseMoved(MouseEvent e) {
+		//Calculates the angle between the mouse-pointer and the player's central coordinates.
 		int angle = (int)Math.toDegrees(Math.atan2(e.getY() - p.getCenterY(), e.getX() - p.getCenterX()));
+		
+		/*
+		 * We need to add 90 degrees because for us, 0 degrees angle is straight up,
+		 * but we calculate it as if it were to the right. Think about the unit circle, (SE Enhetscirkeln).
+		 */
 		angle += 90;
 		
+		//Instead of using negative angles, we just add 360 degrees to get the positive value of the same angle.
 		if(angle < 0){
 			angle += 360;
 		}
