1aa6a1c4854c9c9f0c22e049a1bf99e3320ef59b
William <wilbjo@student.chalmers.se>
2016-03-04 17:07:07 +0100

    Lagt till så att 'type' i ServerGUI matchar typen som är vald: Lava=röd osv

--- src/BAServer/ServerGUI.java
+++ src/BAServer/ServerGUI.java
@@ -79,6 +79,8 @@ public class ServerGUI implements Observer {
 		typeChoice.add("grass");
 		typeChoice.add("lava");
 		typeChoice.add("desert");
+		typeChoice.setForeground(Color.GREEN);
+		typeChoice.setBackground(Color.BLACK);
 
 		// Add components to mainFrame
 		mainWindow.add(serverInfoArea, BorderLayout.NORTH);
